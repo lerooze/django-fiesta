78b137247d106a36a8af267cce240d76d9403ad2
Antonis Loumiotis <al459@columbia.edu>
2019-09-17 20:29:44 +0300

    fix for to compile docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,11 +21,12 @@ sandbox_folder = os.path.realpath(
 sys.path.append(fiesta_folder)
 sys.path.append(sandbox_folder)
 
-# Specify settings module (which will be picked up from the sandbox)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_sphinx')
-
-import django
-django.setup()
+# # Specify settings module (which will be picked up from the sandbox)
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+#
+# import django
+# breakpoint() # BREAKPOINT
+# django.setup()
 
 
 
@@ -40,8 +41,8 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
-    'sphinx_issues',
+    # 'sphinxcontrib.napoleon',
+    # 'sphinx_issues',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -59,8 +60,7 @@ copyright = '2019, Antonis Loumiotis'
 author = 'Antonis Loumiotis'
 
 # The full version, including alpha/beta/rc tags
-from fiesta import __version__
-release = __version__.__version__
+release = '0.5.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -103,49 +103,3 @@ man_pages = [
 
 # Autodoc settings
 autoclass_content = 'class'
-
-import inspect
-from django.utils.html import strip_tags
-from django.utils.encoding import force_text
-
-
-def process_docstring(app, what, name, obj, options, lines):
-    # This causes import errors if left outside the function
-    from django.db import models
-
-    # Only look at objects that inherit from Django's base model class
-    if inspect.isclass(obj) and issubclass(obj, models.Model):
-
-        # Ignore abstract models
-        if not hasattr(obj._meta, '_fields'):
-            return lines
-
-        # Grab the field list from the meta class
-        fields = obj._meta._fields()
-
-        for field in fields:
-            # Decode and strip any html out of the field's help text
-            help_text = strip_tags(force_text(field.help_text))
-
-            # Decode and capitalize the verbose name, for use if there isn't
-            # any help text
-            verbose_name = force_text(field.verbose_name).capitalize()
-
-            if help_text:
-                # Add the model field to the end of the docstring as a param
-                # using the help text as the description
-                lines.append(':param %s: %s' % (field.attname, help_text))
-            else:
-                # Add the model field to the end of the docstring as a param
-                # using the verbose name as the description
-                lines.append(':param %s: %s' % (field.attname, verbose_name))
-
-            # Add the field's type to the docstring
-            lines.append(':type %s: %s' % (field.attname, type(field).__name__))
-
-    # Return the extended docstring
-    return lines
-
-def setup(app):
-    # Register the docstring processor with sphinx
-    app.connect('autodoc-process-docstring', process_docstring)
